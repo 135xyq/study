8cdf6ec68b2efe1e9dd2f20fa87b53a32e205ad9
xie <xyq135@gmail.com>
2021-10-06 20:48:38 +0800

    简易数据爬取更新

---
+++ 包管理器/简易数据爬取/index.js
@@ -0,0 +1,9 @@
+const getMovies = require('./getMovies.js');
+const fs = require('fs');
+
+getMovies.then(movies => {
+    let json = JSON.stringify(movies);
+    fs.writeFile("movies.json", json, function() {
+        console.log(1)
+    });
+})
