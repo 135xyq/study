578752fec0d1ad4d1eb5040ba91bd3a6abe8d0f2
xie <xyq135@gmail.com>
2021-10-24 21:14:48 +0800

    开始vue2

---
+++ vue/vue2/搭建一个工程/my-site/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>APP组件</h1>
+    <Icon type="weixin"></Icon>
+  </div>
+</template>
+
+<script>
+import Icon from "./components/Icon.vue";
+export default{
+  components:{
+    Icon
+  }
+}
+</script>
+
---
+++ vue/vue2/组件/src/App.js
@@ -0,0 +1,5 @@
+const template = `<h1>App组件</h1>`
+
+export default {
+    template,
+};
