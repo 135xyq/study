bcd51de3681bda54983345c4d025400ee0a9093e
xie <xyq135@gmail.com>
2021-12-28 23:50:18 +0800

    完成Todos的新增、筛选和编辑

--- vue/vue3/vue3-study-firstProject/src/App.vue
+++ vue/vue3/vue3-study-firstProject/src/App.vue
@@ -1,15 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div id="app">
+    <section class="todoapp">
+      <header class="header">
+        <h1>todos</h1>
+        <input
+          class="new-todo"
+          autofocus=""
+          autocomplete="off"
+          placeholder="What needs to be done?"
+          v-model = 'newTodoRef'
+          @keyup.enter="addTodo"
+        />
+      </header>
+      <section class="main">
+        <input id="toggle-all" class="toggle-all" type="checkbox" v-model="allDoneRef" />
+        <label for="toggle-all">Mark all as complete</label>
+        <ul class="todo-list">
+          <li class="todo" :class="{completed : todo.completed,editing:todo === editingTodoRef}" v-for="todo in filterTodosRef" :key="todo.id">
+            <div class="view">
+              <input class="toggle" type="checkbox" v-model="todo.completed"/>
+              <label @dblclick="handleEditTodo(todo)">{{todo.title}}</label>
+              <button class="destroy"></button>
+            </div>
+            <input class="edit" type="text" 
+            @blur="doneEdit"
+            @keyup.enter="doneEdit"
+            @keyup.escape="escEdit(todo)"
+            v-model="todo.title"
+            />
+          </li>
+        </ul>
+      </section>
+      <footer class="footer">
+        <span class="todo-count">
+          <strong>{{activeTodosRef}}</strong>
+          <span>item{{activeTodosRef===1?'':'s'}} left</span>
+        </span>
+        <ul class="filters">
+          <li><a href="#/all" :class="{selected:visibilityRef === 'all'}">All</a></li>
+          <li><a href="#/active" :class="{selected:visibilityRef === 'active'}">Active</a></li>
+          <li><a href="#/completed" :class="{selected:visibilityRef === 'completed'}">Completed</a></li>
+        </ul>
+        <button class="clear-completed" v-show = "completedTodosRef > 0">
+          Clear completed
+        </button>
+      </footer>
+    </section>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import  {usetodoList} from './composition/usetodoList.js';
+import useNewTodo from './composition/useNewTodo'
+import useFilter from './composition/useFilter'
+import useEdit from './composition/useEdit'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  setup(){
+    const {todosRef} = usetodoList()
+    return{
+      todosRef,
+      ...useNewTodo(todosRef),
+      ...useFilter(todosRef),
+      ...useEdit(todosRef)
+    }
   }
-}
+};
 </script>
---
+++ vue/vue3/vue3-study-firstProject/src/composition/useEdit.js
@@ -0,0 +1,48 @@
+// 编辑部分
+
+import { ref, computed } from 'vue'
+export default function useEdit(todosRef) {
+    let lastTitle = null; //编辑之前的内容
+    const editingTodoRef = ref(null); //当前修改的任务
+
+    /**
+     * 编辑
+     * @param {*} todo 要编辑的任务
+     */
+    const handleEditTodo = (todo) => {
+        editingTodoRef.value = todo;
+        lastTitle = todo.title;
+    }
+
+    // 完成编辑
+    const doneEdit = () => {
+        editingTodoRef.value = null;
+    }
+
+
+    // 取消编辑
+    const escEdit = (todo) => {
+        todo.title = lastTitle;
+        editingTodoRef.value = null;
+    }
+
+    // 一键全选
+    const allDoneRef = computed({
+        get() {
+            return todosRef.value.filter(item => !item.completed).length === 0;
+        },
+        set(checked) {
+            todosRef.value.forEach(item => {
+                item.completed = checked;
+            })
+        }
+    })
+
+    return {
+        editingTodoRef,
+        handleEditTodo,
+        doneEdit,
+        escEdit,
+        allDoneRef
+    }
+}
