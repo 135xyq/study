63c9bcb5b820e1b51c0685116350caf5a2bbe811
xyq <xyq135@gmail.com>
2022-03-31 19:31:51 +0800

    uniapp-组件的使用

---
+++ uniapp/test_uniapp/components/test/test.vue
@@ -0,0 +1,21 @@
+<template>
+	<view>
+		<icon type="success"></icon>
+		<text>test component</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"test",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style>
+
+</style>
