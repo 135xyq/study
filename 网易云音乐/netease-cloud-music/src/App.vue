4ab2b797d4b30866faa92c3387c7e2f91306b3c2
xyq <xyq135@gmail.com>
2022-04-02 14:38:37 +0800

    网易云音乐-foot页的内容

--- 网易云音乐/netease-cloud-music/src/App.vue
+++ 网易云音乐/netease-cloud-music/src/App.vue
@@ -2,14 +2,17 @@
   <div id="app">
     <Layout></Layout>
     <router-view/>
+    <Foot></Foot>
   </div>
 </template>
 
 <script>
 import Layout from '@/layout';
+import Foot from '@/layout/Foot'
 export default {
   components:{
-    Layout
+    Layout,
+    Foot
   }
 }
 </script>
---
+++ 网易云音乐/netease-cloud-music/src/layout/Foot/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="foot-container">
+      利用网上的接口仿写网易云，感谢
+          <a href="https://github.com/Binaryify/NeteaseCloudMusicApi" target="_blank">https://github.com/Binaryify/NeteaseCloudMusicApi</a>提供的接口
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.foot-container{
+    width: 100%;
+    box-sizing: border-box;
+    background: #F5F5F5;
+    height: 100px;
+    border: 1px solid #ccc;
+    text-align: center;
+    padding-top: 20px;
+}
+
+</style>
