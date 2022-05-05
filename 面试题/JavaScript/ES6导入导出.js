d771d5430d868e122557c33aff3966e2469d5173
xyq <xyq135@gmail.com>
2022-05-05 20:37:37 +0800

    面试题-JavaScript-ES6导入导出

---
+++ 面试题/JavaScript/ES6导入导出.js
@@ -0,0 +1,3 @@
+export const a = 1;
+const b = 32;
+export default 24321421;
---
+++ 面试题/JavaScript/ES6导入导出1.js
@@ -0,0 +1,10 @@
+import * as t from './ES6导入导出.js'
+import t1 from './ES6导入导出.js'
+
+export { c }
+from './ES6导入导出2.js'
+
+console.log(t1)
+
+console.log(t.default)
+console.log(t)
