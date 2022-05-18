1eaa4b60fbaa04b04ea18c7b61626fc4d2a2063b
xyq <xyq135@gmail.com>
2022-05-18 22:27:55 +0800

    react-组件、组件属性和王者荣耀英雄列表渲染

---
+++ react/react-learn/src/TestComponent.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function MyComponent(props){
+    return <h1>函数组件:{props.str}</h1>
+}
--- react/react-learn/src/index.js
+++ react/react-learn/src/index.js
@@ -1,41 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import HeroList from "./components/HeroList";
+
 
 const container = document.getElementById("root");
 
 const root = ReactDOM.createRoot(container);
 
-const imgUrls = ["https://tse3-mm.cn.bing.net/th/id/OIP-C.ZzAuQjvpv1bNI5XAeTnmYwHaJ8?w=192&h=258&c=7&r=0&o=5&dpr=1.56&pid=1.7","https://tse1-mm.cn.bing.net/th/id/OIP-C.h0rWDr3fnmVUvJ8hBqU3_AHaJ4?w=192&h=256&c=7&r=0&o=5&dpr=1.56&pid=1.7","https://tse3-mm.cn.bing.net/th/id/OIP-C.ZbQxr9UfJhh-ePyH9zFJhAHaLM?w=192&h=290&c=7&r=0&o=5&dpr=1.56&pid=1.7","https://tse3-mm.cn.bing.net/th/id/OIP-C.IkmQMM5cPDACsP_r2Yy2QgHaJi?w=192&h=247&c=7&r=0&o=5&dpr=1.56&pid=1.7","https://tse1-mm.cn.bing.net/th/id/OIP-C.bmsqpcZSCqXux2jSVOXc7AHaNB?w=188&h=331&c=7&r=0&o=5&dpr=1.56&pid=1.7"]
-
-let index = 0;//当前显示图片的下标
-let timer = null;//计时器
-
-// 渲染图片
-function render(index){
-	const item = (<img src={imgUrls[index]} alt="" />);
-	root.render(item);
+async function fetchAllHeros(){
+	const heros = await fetch("https://study.duyiedu.com/api/herolist").then(resp=>resp.json()).then(resp=>resp.data);
+	return heros;
 }
 
-// 停止图片的切换
-function stop(){
-	clearInterval(timer);
+async function render(){
+	root.render(<div>获取数据中......</div>)
+	const heros = await fetchAllHeros();
+	root.render((<HeroList heros={heros}></HeroList>))
 }
 
-// 开始切换图片
-function start(){
-	stop();
-	timer = setInterval(()=>{
-		index= (index + 1) % imgUrls.length;
-		render(index);
-	},1000)
-}
 
-start();
+render();
 
-container.onmouseenter = function(){
-	stop();
-}
 
-container.onmouseleave =function(){
-	start();
-}
