6b4dbe7190d5509af8e4b0bef532625dee11084a
xyq <xyq135@gmail.com>
2022-06-17 21:35:52 +0800

    react-reactLearn 属性的类型检查

--- react/react-learn/src/App.js
+++ react/react-learn/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
-import ClassDefaultProps from './components/ClassDefaultProps';
+import PropTypeUse from "./components/PropTypeUse";
 export default class App extends Component {
 	render() {
-		return <ClassDefaultProps></ClassDefaultProps>
+		return <PropTypeUse a="1" b={1} c d={[1, 2, <h1></h1>]} e={{
+			name:"xyq",
+			age:21,
+			sex:"女"
+		}}></PropTypeUse>;
 	}
 }
-
