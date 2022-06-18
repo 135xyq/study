fbcb874c5c4283b20316d7acdab1e7ecbb9de647
xyq <xyq135@gmail.com>
2022-06-18 21:41:54 +0800

    react-reactLearn 通用组件 使用高阶组件优化表单组件

--- react/react-learn/src/App.js
+++ react/react-learn/src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from "react";
-import WithTest  from "./HOC/WithTestHoc";
-import HocTestA from "./components/HocTestA";
-import HocTestB from "./components/HocTestB";
+import Test from "./components/common/FormComHoc/Select/Test"
 
-const TestA = WithTest(HocTestA);
-const TestB = WithTest(HocTestB)
 export default class App extends Component {
 	render() {
 		return (
-			<>
-			<TestA></TestA>
-			<TestB></TestB>
-			</>
+			<Test>
+			</Test>
 		)
 	}
 }
