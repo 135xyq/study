67bad6c2aef3c68d702d922a56010456e3290bd0
xyq <xyq135@gmail.com>
2022-06-27 20:16:03 +0800

    react-reactLearn 修改文件夹

--- react/react-learn/src/App.js
+++ react/react-learn/src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
-import Test from "./components/进阶/RenderProps/Test"
 
 export default class App extends Component {
 	render() {
 		return (
-			<Test>
-			</Test>
+			<div>app</div>
 		)
 	}
 }
