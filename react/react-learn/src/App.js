a45dceec34c61a0aa2755cfe95c700886ff6cbb8
xyq <xyq135@gmail.com>
2022-06-27 21:51:59 +0800

    react-reactLearn Hook-State Hook

--- react/react-learn/src/App.js
+++ react/react-learn/src/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
+import StateHookTest from "./Hook/State Hook"
 
 export default class App extends Component {
-	render() {
-		return (
-			<div>app</div>
-		)
-	}
+  render() {
+	return (
+	  <div><StateHookTest/></div>
+	)
+  }
 }
