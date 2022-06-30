7638636f7295e80ae6f7934afb521221793cf1d0
xyq <xyq135@gmail.com>
2022-06-30 15:23:37 +0800

    刷题-程序员面试金典（第 6 版）面试题 01.07、面试题 01.08、面试题 01.09

---
+++ 刷题/程序员面试金典（第 6 版）/面试题 01.07. 旋转矩阵.js
@@ -0,0 +1,24 @@
+/**https://leetcode.cn/problems/rotate-matrix-lcci/
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+ var rotate = function(matrix) {
+    const n = matrix.length;
+    for (let i = 0; i < Math.floor(n / 2); ++i) {
+        for (let j = 0; j < Math.floor((n + 1) / 2); ++j) {
+            const temp = matrix[i][j];
+            matrix[i][j] = matrix[n - j - 1][i];
+            matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+            matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+            matrix[j][n - i - 1] = temp;
+        }
+    }
+    return matrix;
+};
+
+console.log(rotate([
+    [ 5, 1, 9,11],
+    [ 2, 4, 8,10],
+    [13, 3, 6, 7],
+    [15,14,12,16]
+  ]))
---
+++ 刷题/程序员面试金典（第 6 版）/面试题 01.08. 零矩阵.js
@@ -0,0 +1,41 @@
+/**https://leetcode.cn/problems/zero-matrix-lcci/
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function (matrix) {
+	let arrRow = []; //存放行为0的
+	let arrCol = []; //存放列为0的
+	const m = matrix.length;
+	const n = matrix[0].length;
+	for (let i = 0; i < m; i++) {
+		for (let j = 0; j < n; j++) {
+			if (!arrCol.includes[j]) {
+				if (matrix[i][j] === 0) {
+					if(!arrRow.includes(i)){
+                        arrRow.push(i);
+                    }
+					arrCol.push(j);
+				}
+			}
+		}
+	}
+	for (const item of arrRow) {
+		for (let j = 0; j < n; j++) {
+			matrix[item][j] = 0;
+		}
+	}
+	for (const item of arrCol) {
+		for (let j = 0; j < m; j++) {
+			matrix[j][item] = 0;
+		}
+	}
+	return matrix;
+};
+
+console.log(
+	setZeroes([
+		[0, 1, 2, 0],
+		[3, 4, 5, 2],
+		[1, 3, 1, 5],
+	])
+);
