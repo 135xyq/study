3abd62688ae4b03a3a652a31cc36a2e68645df6f
xyq <xyq135@gmail.com>
2022-07-01 23:56:34 +0800

    react-reactLearn 路由-学生管理系统-获取路由数据

--- react/react-learn/src/App.js
+++ react/react-learn/src/App.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import Test from "./路由/学生管理系统/index"
-import Layout from "././路由/学生管理系统/components/Layout"
+import Test from "./路由/学生管理系统/index";
 
 export default class App extends Component {
-  render() {
-	return (
-		<Router>
-		<Test/>
-		</Router>
-	)
-  }
+	render() {
+		return (
+			<Router>
+				<Test />
+			</Router>
+		);
+	}
 }
--- react/react-learn/src/路由/学生管理系统/index.js
+++ react/react-learn/src/路由/学生管理系统/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { Routes,Route } from 'react-router-dom'
+import { Routes,Route ,useLocation} from 'react-router-dom'
 import Login from "./pages/Login"
 import Admin from "./pages/Admin"
 
 export default function Test() {
+  let location = useLocation ();
+  console.log(location);
   return (
     <Routes>
         <Route path="/login" element={<Login/>}></Route>
