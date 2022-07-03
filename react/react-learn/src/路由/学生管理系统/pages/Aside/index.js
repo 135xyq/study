b679c5a04818abf726791958318ef5e02d88d39e
xyq <xyq135@gmail.com>
2022-07-03 22:07:50 +0800

    react-reactLearn 路由-学生管理系统-设置Link组件

--- react/react-learn/src/路由/学生管理系统/pages/Aside/index.js
+++ react/react-learn/src/路由/学生管理系统/pages/Aside/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import "./index.css"
+import {Link} from "react-router-dom"
 
 export default function Aside() {
   return (
     <ul className='menu'>
-        <li><a href="/student">学生列表</a></li>
-        <li><a href="/student/add">新增学生</a></li>
-        <li><a href="/course">课程列表</a></li>
-        <li><a href="/course/add">新增课程</a></li>
+        <li><Link to="/student">学生列表</Link></li>
+        <li><Link  to="/student/add">新增学生</Link></li>
+        <li><Link to="/course">课程列表</Link></li>
+        <li><Link to="/course/add">新增课程</Link></li>
     </ul>
   )
 }
