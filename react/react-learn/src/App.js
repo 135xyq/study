2104665ec7eb0f4b4cb3a3656524de6c4ccd91b2
xyq <xyq135@gmail.com>
2022-07-07 21:32:43 +0800

    react-reactLearn redux-action

--- react/react-learn/src/App.js
+++ react/react-learn/src/App.js
@@ -1,14 +1,9 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import Test from "./路由/学生管理系统/index";
-import "./redux/firstTest/index"
-
+import "./redux/action/index";
 export default class App extends Component {
 	render() {
 		return (
-			<Router>
-				<Test />
-			</Router>
+			<></>
 		);
 	}
 }
