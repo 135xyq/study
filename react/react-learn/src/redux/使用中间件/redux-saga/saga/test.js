4e1da58b405d6367f79087c663499c962ca615cf
xyq <xyq135@gmail.com>
2022-07-16 16:45:01 +0800

    react-reactLearn redux-使用中间件-saga基础指令

--- react/react-learn/src/redux/使用中间件/redux-saga/saga/test.js
+++ react/react-learn/src/redux/使用中间件/redux-saga/saga/test.js
@@ -1,12 +1,16 @@
-import { take } from "redux-saga/effects"
-import { numberActionTypes } from "../action/number"
+import { delay, put, take, takeEvery, } from "redux-saga/effects"
+import { numberActionTypes ,decrease} from "../action/number"
 
-
+function* testTakeEvery(){
+    yield delay(3000)
+    console.log("延时3秒输出")
+    yield put(decrease());//触发action
+}
 
 export default function* test() {
-    let action1 = yield take(numberActionTypes.increase);
-    console.log("increase",action1)
+    yield takeEvery(numberActionTypes.increase,testTakeEvery)
     let action2 = yield take(numberActionTypes.decrease);
-    console.log("increase",action2)
-
+    console.log("decrease",action2)
+    let action3 = yield take(numberActionTypes.increase);
+    console.log("increase",action3)
 }
