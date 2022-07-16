4c4805ca652bccda03750a981762df25a73bb569
xyq <xyq135@gmail.com>
2022-07-16 22:46:19 +0800

    react-reactLearn 使用中间件-redux-actions

--- react/react-learn/src/App.js
+++ react/react-learn/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./redux/使用中间件/redux-saga/test.js";
+import "./redux/使用中间件/redux-actions/test.js";
 export default class App extends Component {
 	render() {
 		return (
