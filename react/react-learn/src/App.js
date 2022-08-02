c61609f0e01d7fc355d48054ff7439313ec3d05b
xyq <xyq135@gmail.com>
2022-08-02 15:27:26 +0800

    react-reactLearn React Router官网-Routers

--- react/react-learn/src/App.js
+++ react/react-learn/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
-import {BrowserRouter} from "react-router-dom";
-import QuickStart from "./React Router官网/Quick Start"
+import {HashRouter} from "react-router-dom";
+import QuickStart from "./React Router官网/Routers/BrowerRouter"
 
 export default class App extends Component {
 	render() {
 		return (
-			<BrowserRouter >
+			<HashRouter >
 				<QuickStart></QuickStart>
-			</BrowserRouter>
+			</HashRouter>
 		);
 	}
 }
