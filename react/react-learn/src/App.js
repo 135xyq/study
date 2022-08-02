7f0fce4a3c89bea48a079f2270eafefbd3e82373
xyq <xyq135@gmail.com>
2022-08-02 11:18:59 +0800

    react-reactLearn React Router官网-Installation

--- react/react-learn/src/App.js
+++ react/react-learn/src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from "react";
-import {Provider} from "react-redux"
-import Test from "./路由/学生管理系统"
-import {Route} from "react-router-dom"
-import store ,{history}from "./redux/使用中间件/redux-saga"
-import {BrowserRouter as Router} from "react-router-dom"
-import { ConnectedRouter } from 'connected-react-router'
+import {BrowserRouter} from "react-router-dom";
+import Installation from "./React Router官网/Installation"
+
 export default class App extends Component {
 	render() {
 		return (
-			<Provider store={store}>
-				<Router history={history}>
-					<Test></Test>
-				</Router>
-			</Provider>
+			<BrowserRouter >
+				<Installation></Installation>
+			</BrowserRouter>
 		);
 	}
 }
