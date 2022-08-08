27c390f4f506a4d77de8733d18f0bb24c78929fd
xyq <xyq135@gmail.com>
2022-08-08 11:09:32 +0800

    vue-vue官网vue2 learn-模板语法

--- vue/vue官网学vue2/learn/src/views/Home.vue
+++ vue/vue官网学vue2/learn/src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png" />
+		<div class="title">
+			<span v-bind:title="'title'">提示标题</span>
+		</div>
+		<div class="reverse">
+			<p>{{message}}</p>
+			<button @click="onHandleReverseMessage">反转字符串</button>
+		</div>
+		<div class="input">
+			<p>{{inputVal}}</p>
+			<input type="text" v-model="inputVal">
+		</div>
+		<div class="once">
+			<p v-once>{{onceData}}</p>
+		</div>
+		<div>
+			<a v-bind:[testAttribute]="'/about'">about</a>
+		</div>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+	name: "Home",
+	data(){
+		return {
+			message:'hello world!',
+			inputVal:"test",
+			onceData:"不会改变的数据",
+			testAttribute:"href"
+		}
+	},
+	beforeCreate(){
+		// console.log(this)
+	},
+	methods:{
+		onHandleReverseMessage(){
+			this.message = this.message.split('').reverse().join('');
+		}
+	}
+};
 </script>
