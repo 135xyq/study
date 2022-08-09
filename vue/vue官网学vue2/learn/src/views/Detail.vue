7ce5e26c22c3723befd86474264c138e742c86b1
xyq <xyq135@gmail.com>
2022-08-09 17:03:18 +0800

    vue-vue官网vue2 learn-Router

---
+++ vue/vue官网学vue2/learn/src/views/Detail.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="detail-container">
+    <router-link :to="{name:'Layout'}">layout</router-link>
+    <router-view name="Header"></router-view>
+    <router-view name="Main"></router-view>
+    <router-view name="Foot"></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
