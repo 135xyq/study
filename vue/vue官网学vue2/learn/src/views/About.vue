8c7e4a5fce8aadf4881fe28f6815600a946d8d09
xyq <xyq135@gmail.com>
2022-08-09 21:24:21 +0800

    vue-vue官网vue2 learn-vuex

--- vue/vue官网学vue2/learn/src/views/About.vue
+++ vue/vue官网学vue2/learn/src/views/About.vue
@@ -2,13 +2,18 @@
 	<div class="about">
 		<h1>This is an about page</h1>
 		<button v-on:click="onHandleToHome">首页</button>
+		<Count></Count>
 	</div>
 </template>
 
 <script>
+import Count from "@/components/Count";
 export default {
+	components:{
+		Count
+	},
 	created(){
-		console.log(this.$router,this.$route)
+		// console.log(this.$router,this.$route)
 	},
 	methods:{
 		onHandleToHome(){
