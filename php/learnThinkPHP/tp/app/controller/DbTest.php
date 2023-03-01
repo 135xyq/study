7ed2b65c8c20af5f896ef68530e85e7e8929f6bc
xyq <xyq135@gmail.com>
2023-03-01 17:25:21 +0800

    php-learnThinkPHP-tp 数据库修改和删除

--- php/learnThinkPHP/tp/app/controller/DbTest.php
+++ php/learnThinkPHP/tp/app/controller/DbTest.php
@@ -7,10 +7,10 @@ use think\facade\Db;
 
 class DbTest
 {
+//    插入
     public function insert()
     {
         $data = [
-            'id' => 79,
             'username' => '光头强',
             'password' => '123',
             'gender' => '男',
@@ -19,5 +19,28 @@ class DbTest
             'details' => '123'
         ];
         return Db::name('user')->save($data);
+    }
+
+//    修改
+    public function update()
+    {
+        $data = [
+            'username' => '黑小虎'
+        ];
+//        return Db::name('user')->where('id',301)->update($data);
+//        return Db::name('user')->where('id',301)->exp('email','UPPER(email)')->update();
+//        return Db::name('user')->where('id',301)
+//            ->inc('price',5)
+//            ->dec('status')
+//            ->update();
+        Db::name('user')->where('id', 302)
+            ->update([
+                'email' => Db::raw('UPPER(email)'), 'price' => Db::raw('price + 1'), 'status' => Db::raw('status - 2')]);
+    }
+
+    public function delete()
+    {
+//        return Db::name('user')->delete(304);
+        return Db::name('user')->where('id',305)->delete();
     }
 }
