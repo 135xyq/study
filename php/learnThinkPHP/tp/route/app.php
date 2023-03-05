2c2327997229e75db6b79db8f7a31f9a05e081d0
xyq <xyq135@gmail.com>
2023-03-05 16:24:13 +0800

    php-learnThinkPHP-tp 请求缓存

--- php/learnThinkPHP/tp/route/app.php
+++ php/learnThinkPHP/tp/route/app.php
@@ -14,7 +14,7 @@ use think\facade\Route;
 //    return 'hello,ThinkPHP6!';
 //});
 //
-//Route::get('hello/:name', 'index/hello');
+Route::get('hello/:name', 'index/hello');
 
 //Route::rule('ds/:id','address/detail')->pattern(['id'=>'\d+']);
 
