f2d15b7c4df5f21e1f08789bddf25041547767ca
谢永强 <xyq135@gmail.com>
2023-11-07 17:20:05 +0800

    TypeScript 基础 基本类型

--- TypeScript/基础/src/baseType/index.ts
+++ TypeScript/基础/src/baseType/index.ts
@@ -1,4 +1,61 @@
-let say:string = "hello"
+let str: string = 'hello world';
 
-console.log(212)
-console.log(12)
+let num: number = 12;
+
+let bool: boolean = false;
+
+let any: any = [false]
+
+function hello(str: string) {
+  console.log('hello', str)
+}
+
+hello('word')
+
+function printCord(pt: { x: number; y: number }) {
+  console.log("The coordinate's x value is " + pt.x);
+  console.log("The coordinate's y value is " + pt.y);
+}
+
+printCord({x: 3, y: 7});
+
+function printId(id: number | string | boolean) {
+  console.log("Your ID is: " + id);
+}
+
+printId(101);
+printId("202");
+printId(false)
+
+type Point = {
+  x: number | string,
+  y: number
+}
+
+function testPoint(pt: Point) {
+  console.log(pt.x, pt.y);
+}
+
+testPoint({x: '111', y: 11})
+
+interface interfaceTestPoint {
+  x: number | string,
+  y: number | string
+}
+
+function testInterfacePoint(pt: Point) {
+  console.log(pt.x, pt.y);
+}
+
+testInterfacePoint({x: 1, y: 2})
+
+interface testExtend extends interfaceTestPoint {
+  z: number | string
+}
+
+function testInterfaceExtendPoint(pt: testExtend) {
+  console.log(pt.x, pt.y, pt.z);
+}
+
+testInterfaceExtendPoint({x: 1, y: 2, z: '3'})
+
