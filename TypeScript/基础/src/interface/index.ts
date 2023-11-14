2d8c03c463474768158cee105fd7e88e3c9e1902
谢永强 <xyq135@gmail.com>
2023-11-14 09:50:01 +0800

    TypeScript 基础 接口

--- TypeScript/基础/src/interface/index.ts
+++ TypeScript/基础/src/interface/index.ts
@@ -5,8 +5,22 @@ interface User {
 
 let u: User = {
   name: 'xyq',
-  age: 22
+  age: 22,
 }
 
+interface LabelledValue {
+  label: string;
+}
 
-console.log(u)
+function printLabel(labelledObj: LabelledValue) {
+  console.log(labelledObj.label);
+}
+
+let myObj = {size: 10, label: "Size 10 Object"};
+printLabel(myObj);
+
+interface SquareConfig {
+  color?: string;
+  width?: number;
+}
+
